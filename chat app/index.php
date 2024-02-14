5618e044258bb0d95ea44e7d44602f59febe8070
yahia3892 <144722513+yahia3892@users.noreply.github.com>
2024-02-14 19:30:51 +0000

    chat app
    hi ther

---
+++ chat app/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>chat app</title>
+</head>
+<body>
+  <div id ="container">
+  <div id ="chatbox">
+  <samp>yhia
+  </samp>
+  <samp>: </samp>
+  <samp>Hallo how are you</samp>
+  <div id ="chatdata"></div>
+  </div>
+  </div>
+</body>
+</html>
